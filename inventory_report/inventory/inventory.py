d19c27cb0337d575cc002c98a86ae2059f5f0e3e
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-31 21:54:00 -0300

    Feat: build json reader file and start xml reader file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,24 @@
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 import csv
+import json
 
 
 class Inventory(CompleteReport, SimpleReport):
     @classmethod
     def import_data(cls, path, type):
-        with open(path) as file:
-            reports = csv.DictReader(file, delimiter=",", quotechar='"')
-            read = [report for report in reports]
+        read = []
+        if path.lower().endswith('.csv'):
+            with open(path) as file:
+                reports = csv.DictReader(file, delimiter=",", quotechar='"')
+                read = [report for report in reports]
+        elif path.lower().endswith('.json'):
+            with open(path) as file:
+                read = json.load(file)
+        elif path.lower().endswith('.xml'):
+            with open(path) as file:
+                read = file.read()
+
         if type == 'simples':
             return SimpleReport.generate(read)
         else:
